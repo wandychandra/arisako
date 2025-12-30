785ecb21b7198d8605db9fbea826b59a61337b09
Husni Abdillah <ray25husni@apps.ipb.ac.id>
2025-12-30 12:27:11 +0700

    chore: update TypeScript configuration and add Next.js config file

--- app/rootProvider.tsx
+++ app/rootProvider.tsx
@@ -1,25 +1,25 @@
 "use client";
 import { ReactNode } from "react";
-import { base } from "wagmi/chains";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
 import "@coinbase/onchainkit/styles.css";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { base } from "wagmi/chains";
+import { config } from "@/lib/web3/config";
+
+const queryClient = new QueryClient();
 
 export function RootProvider({ children }: { children: ReactNode }) {
   return (
-    <OnchainKitProvider
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={base}
-      config={{
-        appearance: {
-          mode: "auto",
-        },
-        wallet: {
-          display: "modal",
-          preference: "all",
-        },
-      }}
-    >
-      {children}
-    </OnchainKitProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <OnchainKitProvider
+          apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+          chain={base}
+        >
+          {children}
+        </OnchainKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
